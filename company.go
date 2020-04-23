865cd61ae546a3d08606933a3c4cbc8d2f8177bc
Marcelo Monaco <marcelo.monaco@gmail.com>
2020-04-23 15:19:20 -0300

    Testando possibilidade de retorno do status code 100 no upload de certificado

--- company.go
+++ company.go
@@ -217,7 +217,7 @@ func (c *Client) UploadCertificate(id, file, password string) error {
 	}
 
 	// check response status code
-	if len(header) < 1 || !strings.Contains(header[0], "200") {
+	if len(header) < 1 || NotIn(header[0], "100", "200") {
 
 		// get status code
 		data := strings.Split(strings.Trim(header[0], " "), " ")
